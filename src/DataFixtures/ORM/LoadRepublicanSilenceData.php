d6295d3db34d304c0d65eab40917febc2c66f5ca
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2020-03-11 17:24:21 +0100

    Fix phantom deputy test

--- src/DataFixtures/ORM/LoadRepublicanSilenceData.php
+++ src/DataFixtures/ORM/LoadRepublicanSilenceData.php
@@ -12,7 +12,6 @@ class LoadRepublicanSilenceData extends AbstractFixture implements DependentFixt
     public function load(ObjectManager $manager)
     {
         $entity = new RepublicanSilence();
-        $entity->addReferentTag($this->getReference('referent_tag_75001'));
         $entity->addReferentTag($this->getReference('referent_tag_91'));
         $entity->addReferentTag($this->getReference('referent_tag_93'));
         $entity->addReferentTag($this->getReference('referent_tag_sg'));
